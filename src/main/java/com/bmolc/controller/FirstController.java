1911c8cdbef0e6407cc89aea0be2c7288c4f9189
Boris <b.molchanov.90@gmail.com>
2025-02-26 12:23:23 +0300

    lesson 4 Model, Calculator

--- src/main/java/com/bmolc/controller/FirstController.java
+++ src/main/java/com/bmolc/controller/FirstController.java
@@ -1,6 +1,8 @@
 package com.bmolc.controller;
 
+import com.bmolc.mathMethods.Calculator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,21 +13,15 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
 
-//    @GetMapping("/hello")
-//    public String helloPage(HttpServletRequest request) {
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
-
-//    @GetMapping("/hello")
-//    public String helloPage(@RequestParam("name") String name,
-//                            @RequestParam("surname") String surname) {
-
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.printf("Hello %s %s!", name, surname);
-        System.out.println();
+//        System.out.printf("Hello %s %s!", name, surname);
+//        System.out.println();
+        String answer = String.format("Hello %s %s!", name, surname);
+        model.addAttribute("message", answer);
 
         return "/first/hello";
     }
@@ -33,5 +29,26 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "/first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a") int a,
+                                 @RequestParam(value = "b") int b,
+                                 @RequestParam(value = "action") String action,
+                                 Model model) {
+        double answer = 0;
+        Calculator calculator = new Calculator(a, b);
+
+        switch (action) {
+            case "multiplication": answer = calculator.multiplication(); break;
+            case "addition": answer = calculator.addition(); break;
+            case "subtraction": answer = calculator.subtraction();break;
+            case "division": answer = calculator.division();break;
+        }
+
+        String resultStr = String.format("Your result = %d", answer);
+        model.addAttribute("result", resultStr);
+
+        return "/first/calculator";
     }
 }
