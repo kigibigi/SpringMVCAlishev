19e27f8c64156be0aeddbe2f3f9a82528a249d41
Boris <b.molchanov.90@gmail.com>
2025-03-04 22:31:33 +0300

    lesson 9 @ModelAttribute, Create Person

--- src/main/java/com/bmolc/controller/PeopleController.java
+++ src/main/java/com/bmolc/controller/PeopleController.java
@@ -1,12 +1,11 @@
 package com.bmolc.controller;
 
 import com.bmolc.dao.PersonDAO;
+import com.bmolc.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -31,6 +30,17 @@ public class PeopleController {
                        Model model) {
         // Поулчим одного человека по его id из DAO и передади этого человека в представление
         model.addAttribute("person", personDAO.show(id));
-        return "/show";
+        return "/people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "/people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return  "redirect:/people";
     }
 }
